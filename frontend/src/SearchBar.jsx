ad0089f1ccbae9319f7fd57e90d934b0accc0f0e
jackie-jia <jacqueline.jia2@gmail.com>
2023-01-10 22:54:29 -0600

    fix api url

--- frontend/src/SearchBar.jsx
+++ frontend/src/SearchBar.jsx
@@ -37,7 +37,7 @@ export default function SearchBar(props) {
         const filters = createFilters();
         const config = {
           method: 'post',
-          url: 'http://aprimavista.pythonanywhere.com/pieces/filter',
+          url: 'https://aprimavista.pythonanywhere.com/pieces/filter',
           headers: { 
             'Content-Type': 'application/json'
           },
